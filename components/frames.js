2854fd9ac2f57ea50bdca6e08a28b9e3f99e5715
Aiden Robinson <96023835+Common-Crow23@users.noreply.github.com>
2021-12-23 21:12:06 -0500

    Update

--- components/frames.js
+++ components/frames.js
@@ -8,7 +8,7 @@ var myGameArea = {
     this.interval = setInterval(updateGameArea, 20);
   },
   clear : function() {
-    this.context.clearReact(0, 0, this.canvas.width, this.canvas.height);
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
 }
 
@@ -20,7 +20,7 @@ function component(width, height, color, x, y) {
   this.update = function(){
     ctx = myGameArea.context;
     ctx.fillStyle = color;
-    ctx.fillReact(this.x, this.y, this.width, this,height);
+    ctx.fillRect(this.x, this.y, this.width, this,height);
   }
 }
 
