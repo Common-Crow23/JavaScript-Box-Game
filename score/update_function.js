00def1647514a44b5f84d1ff85186d75515d8d8e
Common_Crow23 <96023835+Common-Crow23@users.noreply.github.com>
2021-12-24 08:38:04 -0500

    Create update

---
+++ score/update_function.js
@@ -0,0 +1,21 @@
+function component(width, height, color, x, y, type) {
+  this.type = type;
+  this.width = width;
+  this.height = height;
+  this.speedX = 0;
+  this.speedY = 0; 
+  this.x = x;
+  this.y = y; 
+  this.update = function() {
+    ctx = myGameArea.context;
+    if (this.type == "text") {
+      ctx.font = this.width + " " + this.height;
+      ctx.fillStyle = color;
+      ctx.fillText(this.text, this.x, this.y);
+    } else {
+      ctx.fillStyle = color;
+      ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+  }
+...
+}
