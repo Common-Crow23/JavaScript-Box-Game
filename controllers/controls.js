8cedd7a4035cc9298736c05869ce065dfaf1f38b
Aiden Robinson <96023835+Common-Crow23@users.noreply.github.com>
2021-12-23 21:24:39 -0500

    Create controls.js

--- controllers/controls.js
+++ controllers/controls.js
@@ -9,4 +9,38 @@ function component(width, height, color, x, y) {
   this.update = function() {
     ctx = myGameArea.context;
     ctx.fillStyle = color;
-    ctx.fillRect
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+  this.newPos = function() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+  }
+}
+
+function updateGameArea() {
+  myGameArea.clear();
+  myGamePiece.newPos();
+  myGamePiece.update();
+}
+
+function moveup() {
+  myGamePiece.speedY -= 1;
+}
+
+function movedown() {
+  myGamePiece.speedY += 1;
+}
+
+function moveleft() {
+  myGamePiece.speedX -=1;
+}
+
+function moveright() {
+  myGamePiece.speedX += 1;
+}
+<script>
+
+<button onclick="moveup()">UP</button>
+<button onclick="movedown()">DOWN</button>
+<button onclick="moveleft()">LEFT</button>
+<button onclick="moveright()">RIGHT</button>
