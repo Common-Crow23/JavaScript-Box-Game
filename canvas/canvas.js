8e05b76a74b95c02915e8a1d41b6a108ec8480ef
Aiden Robinson <96023835+Common-Crow23@users.noreply.github.com>
2021-12-23 20:48:54 -0500

    Create canvas.js

---
+++ canvas/canvas.js
@@ -0,0 +1,10 @@
+function startGame() {
+  myGameArea.start();
+}
+
+var myGameArea = {
+  canvas : document.createElement("canvas"),
+  start : function() {
+    this.canvas.width = 480;
+    this.canvas.height = 270;
+    this.context = this.canvas.getContext
