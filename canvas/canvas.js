434ee763d76937e818549c027306cb2964bebfdf
Aiden Robinson <96023835+Common-Crow23@users.noreply.github.com>
2021-12-23 20:50:33 -0500

    Update canvas.js

--- canvas/canvas.js
+++ canvas/canvas.js
@@ -7,4 +7,7 @@ var myGameArea = {
   start : function() {
     this.canvas.width = 480;
     this.canvas.height = 270;
-    this.context = this.canvas.getContext
+    this.context = this.canvas.getContext("2d");
+    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+  }
+}
